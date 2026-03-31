ed1f7c27894cf620256d4e77a4f20d024e708261
jlingford <james.lingford@gmail.com>
2026-03-31 19:17:47 +1100

    docs

--- scripts/gbk_renamer.sh
+++ scripts/gbk_renamer.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
 
+# Renames .gbk files to make clinker output easier to read
+#
+# TODO:
+# - [ ] add usage{}
+
 # path to target dir of .gbk files for clinker
-dir_base=$1
+DIR_BASE=$1
 # path to HydDB classification output
 # NOTE: field 2 should be the HydDB group name
-group_map=$2
+GROUP_MAP=$2
 
 # loop over files in dir
-for gbk in "${dir_base}"/*.gbk; do
+for gbk in "${DIR_BASE}"/*.gbk; do
     if [[ -f $gbk ]]; then
+
+        # assumes an input file name of 'GENOMEID___GENEID-genbank_region.gbk'
 
         # get stem name and remove suffix
         gbkname="${gbk##*/}"
@@ -20,20 +27,20 @@ for gbk in "${dir_base}"/*.gbk; do
         gene_id=$(echo "$gbkname" | awk -F "___" '{print $2}')
 
         # get the gene_id's corresponding HydDB group from DIAMOND output
-        hydgroup=$(grep "$gene_id" "$group_map" | cut -f2)
+        hydgroup=$(grep "$gene_id" "$GROUP_MAP" | cut -f2)
         # species=$(grep "$genome_name" "$tax_map" | awk -F";" '{print $NF}' | sed 's/s__//' | sed 's/ /_/')
 
         # set new filename
         new_gbk_name="${hydgroup}___${genome_id}___${gene_id}.gbk"
 
         # new path name
-        new_dir_name="${dir_base}/${new_gbk_name}"
+        new_dir_name="${DIR_BASE}/${new_gbk_name}"
 
         # dry run test
         echo "$new_dir_name"
 
         # WARN: renames files
-        # mv "$gbk" "$new_dir_name"
+        mv "$gbk" "$new_dir_name"
 
     fi
 done
